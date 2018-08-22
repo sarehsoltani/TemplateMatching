b2171977d352dc4e07fad2bb4b3613bfe914b66c
Sare Soltani <sare.soltani96@yahoo.com>
2018-08-23 00:34:51 +0430

    Added GetMatch function

--- TemplateMatching/TemplateMatching.c
+++ TemplateMatching/TemplateMatching.c
@@ -6,6 +6,34 @@
 #include <omp.h>
 #include "bmp_util.h"
 
+////////////////////////////////////////////////////////////////////////////////
+int getMatch(float *I, float *T, int Iw, int Ih, int Tw, int Th) {
+	float *differences;
+	float diff = 0;
+	float temp;
+	int counter = 0;
+	size_t difference_size = sizeof(float) * (Iw - Tw + 1) * (Ih - Th + 1);
+	differences = (float *)malloc(difference_size);
+	int i, j, k, l;
+	//move template on Image and  calculate differences between each pixels
+	for (i = 0; i <= Ih - Th; i++) {
+		for (j = 0; j <= Iw - Tw; j++) {
+			for (k = 0; k < Th; k++) {
+				for (l = 0; l < Tw; l++) {
+					temp = I[((l + j) + (k + i)*Iw)] - T[l + k*Tw];
+					diff += fabsf(temp);
+				}
+			}
+			differences[j + i * (Iw - Tw + 1)] = (diff / ((float)(Th*Tw)));
+			diff = 0;
+		}
+	}
+	//find min
+	counter = getMinimum(differences, (Iw - Tw + 1), (Ih - Th + 1), Th*Tw);
+	return counter;
+}
+///////////////////////////////////////////////////////////////////////////////
+
 int main(int argc, char *argv[]) {
 	int I_width, I_height, T_width, T_height;
 	float *I, *T;
