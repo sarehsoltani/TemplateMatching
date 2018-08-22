ff825e8a2d6bfdc0f25b727297dd95e577a65cda
Sare Soltani <sare.soltani96@yahoo.com>
2018-08-23 00:06:25 +0430

    check size of entrance pic

--- TemplateMatching/TemplateMatching.c
+++ TemplateMatching/TemplateMatching.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[]) {
 	int match_count = 0;
 	int argIter = 1;
 	int argc_temp = argc;
+	//get image and template from user
 	printf("Format: templatmatching.exe image1.bmp template1.bmp image2.bmp template2.bmp ... \n");
 	argc_temp--;
 	I = ReadBMP(argv[1], &I_width, &I_height);
@@ -19,4 +20,15 @@ int main(int argc, char *argv[]) {
 	argIter++;
 	T = ReadBMP(argv[2], &T_width, &T_height);
 	argc_temp--;
+	//check Image size & Template size and compare with each other
+	if (I == 0 || T == 0) {
+		exit(1);
+	}
+
+	while (I != 0 && T != 0) {
+		if (I_width < T_width || I_height < T_height) {
+			fprintf(stderr, "Error: The template is larger than the picture\n");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
